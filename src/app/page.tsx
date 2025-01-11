3f52184f1c1b141cf59e3d8b6566280545b00264
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-11 12:06:14 -0300

    Add Home page component to structure main layout with Hero, AboutMe, Work, Testimonials, Companies, and Footer sections

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+
+import AboutMe from "@/components/sections/journey";
+import Footer from "@/components/layout/footer";
+import Hero from "@/components/sections/hero";
+import Testimonials from "@/components/sections/testimonials";
+import Work from "@/components/sections/work";
+import Companies from "@/components/general/companies";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Head>
+        <meta property="og:image" content="/images/open-graph-ricardo.png" />
+      </Head>
+      <Hero />
+      <AboutMe />
+      <Work />
+      <Testimonials />
+      <Companies />
+      <Footer />
+    </main>
+  );
+}
