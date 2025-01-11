9a087c76bcdf3ac5ab6911c2a26c43c5ce0d4681
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-11 12:31:55 -0300

    Add Photography component to showcase a curated collection of images with descriptive text and responsive layout

---
+++ src/components/sections/photography.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+
+import { PHOTOS } from '@/lib/data';
+import Typography from '@/components/general/typography';
+
+const Photography = () => {
+  return (
+    <section className="w-full py-24 px-10 max-md:px-4 flex flex-col gap-16 items-center max-md:py-16 justify-center">
+      <div className="flex flex-col justify-center gap-4 items-center max-w-7xl">
+        <Typography variant="h2" component="h2">
+          PHOTOGRAPHY
+        </Typography>
+        <Typography variant="body1" className="text-center">
+          Explore a mix of moments that catch my eye – from candid shots to
+          peaceful landscapes. It’s all about seeing things from a fresh
+          perspective. <br />
+          Dive in and enjoy!
+        </Typography>
+      </div>
+      <div className="flex flex-wrap gap-5 justify-center">
+        {PHOTOS.map((photo) => (
+          <Image
+            src={photo.photo}
+            alt={photo.alt}
+            key={photo.alt}
+            width={590}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Photography;
