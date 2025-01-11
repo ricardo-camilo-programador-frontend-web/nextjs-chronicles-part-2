e2a6969c9ed83205696ffafbf114edfbcc9c8b17
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-11 11:52:34 -0300

    Add Testimonials component to showcase user experiences with client feedback and avatars

---
+++ src/components/sections/testimonials.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { TESTIMONIALS } from '@/lib/data';
+import Client from '@/components/general/client';
+import Typography from '@/components/general/typography';
+
+const Testimonials = () => {
+  return (
+    <section
+      className="w-full flex justify-center items-center flex-col py-24 px-4 gap-24 bg-zinc-50 max-md:py-16"
+      id="testimonials">
+      <Typography variant="h2" className="uppercase w-full flex justify-center">
+        Experiências Memoráveis
+      </Typography>
+      <div className="flex gap-16 max-lg:flex-col">
+        {TESTIMONIALS.map((testimonial) => (
+          <React.Fragment key={testimonial.name}>
+            <Client
+              name={testimonial.name}
+              testimonial={testimonial.testimonial}
+              avatar={testimonial.avatar}
+              company={testimonial.company}
+            />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
