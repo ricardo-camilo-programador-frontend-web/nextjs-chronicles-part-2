5ded6128768fa02fac28a5a1c32831132cf9de3d
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-11 14:44:00 -0300

    Enhance Skill component: Update styling to improve user interaction with hover and active states, and adjust icon size for better visibility.

--- src/components/general/skill.tsx
+++ src/components/general/skill.tsx
@@ -11,8 +11,8 @@ type SkillProps = {
 
 const Skill = ({ variant = 'sm', label, icon }: SkillProps) => {
   return (
-    <div className={'px-2 flex gap-2 border border-zinc-200 rounded py-1'}>
-      <Image src={icon} alt={label} />
+    <div className={'px-2 flex items-center gap-2 border border-zinc-200 rounded py-1 hover:bg-zinc-100 active:bg-zinc-200'}>
+      <Image src={icon} alt={label} className="w-6 h-6" />
       <Typography variant={variant === 'sm' ? 'body2' : 'body1'}>
         {label}
       </Typography>
