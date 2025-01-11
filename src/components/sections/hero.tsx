ed67f451a76fbcd811d1d668ebb72f3080fa3a3e
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-11 10:00:35 -0300

    Add Hero component to showcase developer profile with image, typography, and social links

---
+++ src/components/sections/hero.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+
+import Typography from '@/components/general/typography';
+import Social from '@/components/general/social';
+import { StarRating } from '@/components/StarRating';
+import Ricardo from '/public/images/Ricardo-Camilo - Frontend - Developer - Frontend - Engineer - Software - Engineer - Web - Developer - Vuejs - Vue - Reactjs - React - JavaScript - TypeScript.png';
+
+const Hero = () => {
+  return (
+    <section className="w-full pb-24 pt-16 flex justify-center max-lg:py-16 max-lg:pt-0">
+      <div className="w-full max-w-[1120px] px-8 flex justify-between items-center max-lg:max-full max-lg:flex-col max-lg:p-4 max-lg:pt-0 max-xl:w-full gap-8">
+        <div className="flex flex-col gap-12 max-lg:order-1">
+          <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <StarRating rating={5} /> | 4 anos de experiência
+              </div>
+              <Typography
+                variant="h1"
+                className="uppercase bg-gradient-to-r from-yellow-50 to-rose-100 bg-[length:100%_14px] bg-no-repeat bg-bottom w-fit">
+                RICARDO CAMILO
+              </Typography>
+            </div>
+            <div className="flex gap-1 flex-col max-w-2xl">
+              <Typography variant="body1" className="font-medium">
+                Frontend Developer especializado em Frameworks JavaScript
+              </Typography>
+              <Typography variant="body1" className="font-medium">
+                Transformando ideias em experiências digitais excepcionais
+              </Typography>
+            </div>
+          </div>
+          <Social variant="light" />
+        </div>
+        <div className="overflow-hidden rounded-[168px] rounded-bl max-lg:order-0 max-lg:rounded-[40px] max-lg:rounded-bl max-h-[450px] max-md:max-h-auto">
+          <Image src={Ricardo} alt="Riddhi's photo" width={400} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
