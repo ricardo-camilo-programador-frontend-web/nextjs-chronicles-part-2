f35072a1f91ec73fc0d33cd19e4eb81ba2ae9e35
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-13 15:32:51 -0300

    Refactor Header component: Simplify navigation structure by integrating HeaderElements for dynamic link rendering. This update enhances code maintainability and improves user experience by streamlining the header layout while retaining essential navigation features.

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,100 +1,28 @@
-'use client';
-
-import { usePathname, useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
-
-import { Menu, Xmark } from 'iconoir-react';
-import { twMerge } from 'tailwind-merge';
-
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerTrigger,
-} from '@/components/general/drawer';
-import Link, { LinkProps } from '@/components/general/link';
-import { NAV_LINKS } from '@/lib/data';
-import { LanguageSwitcher } from '@/components/LanguageSwitcher';
-import { locales } from '@/config/i18n-config';
+import HeaderElements from '@/blocks/HeaderElements';
 
 const Header = () => {
-  const pathname = usePathname();
-
-  const homePaths = ['/', ...locales.map(locale => `/${locale}`)];
-  const isHomePage = homePaths.includes(pathname);
-
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const renderHomePageSectionNavLinks = () => {
-    return NAV_LINKS.map((link: LinkProps) => (
-      <li key={link.children?.toLocaleString()}>
-        <Link href={link.href}>{link.children}</Link>
-      </li>
-    ));
-  };
-
-  useEffect(() => {
-    setIsOpen(false);
-  }, [pathname]);
+  const navLinks = [
+    { label: 'journey', href: '#journey' },
+    { label: 'work', href: '#work' },
+    { label: 'testimonials', href: '#testimonials' },
+    { label: 'contact', href: '#contact' },
+  ];
 
   return (
-    <>
-      <header className="flex p-8 justify-center max-lg:px-4 sticky top-0 bg-white z-50">
-        <div className="flex max-w-7xl w-full justify-between">
-          <a
-            href="/"
-            className="text-xl font-medium uppercase text-zinc-900 tracking-[1px] cursor-pointer"
-            rel="home"
-            aria-label="Ricardo Camilo"
-          >
-            RICARDO CAMILO
-          </a>
+    <header className="flex p-8 justify-center max-lg:px-4 sticky top-0 bg-white z-50">
+      <div className="flex max-w-7xl w-full justify-between">
+        <a
+          href="/"
+          className="text-xl font-medium uppercase text-zinc-900 tracking-[1px] cursor-pointer"
+          rel="home"
+          aria-label="Ricardo Camilo"
+        >
+          RICARDO CAMILO
+        </a>
 
-          {isHomePage && (
-            <ul className="flex gap-8 max-md:hidden max-lg:gap-4">
-              {renderHomePageSectionNavLinks()}
-            </ul>
-          )}
-          <div className="flex gap-8 items-center">
-            <div className="my-auto">
-              <LanguageSwitcher />
-            </div>
-
-            <Link
-              href={isHomePage ? '/beyond-bio' : '/'}
-              className={isHomePage ? 'max-md:hidden' : 'max-sm:hidden'}>
-              {isHomePage ? 'Além da bio' : 'Início'}
-            </Link>
-          </div>
-          <Drawer open={isOpen} onOpenChange={setIsOpen}>
-            <DrawerTrigger
-              asChild
-              className={twMerge(isHomePage ? 'flex md:hidden' : 'sm:hidden')}>
-              <Menu />
-            </DrawerTrigger>
-            <DrawerContent>
-              <div className="flex items-center justify-between border-b border-zinc-100 p-4 bg-white">
-                <p className="text-base font-medium uppercase text-zinc-900 tracking-[1px]">
-                  RICARDO CAMILO
-                </p>
-                <DrawerClose asChild>
-                  <Xmark height={20} width={20} />
-                </DrawerClose>
-              </div>
-              <div className="border-b border-zinc-100 p-4 bg-white h-screen">
-                <ul className="flex flex-col gap-2 max-lg:gap-4">
-                  <li className="py-4">
-                    <Link href={isHomePage ? '/beyond-bio' : '/'}>
-                      {isHomePage ? 'Além da bio' : 'Início'}
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </DrawerContent>
-          </Drawer>
-        </div>
-      </header>
-    </>
+        <HeaderElements navLinks={navLinks} />
+      </div>
+    </header>
   );
 };
 
