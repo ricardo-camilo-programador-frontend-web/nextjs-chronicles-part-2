b26be6d166eb11f7baf96cc5bf8776d5ac4f7d54
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-13 10:08:57 -0300

    Integrate next-intl for internationalization: Update Next.js configuration to include next-intl plugin, enhancing locale support and message handling.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
     domains: [
@@ -10,4 +13,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
