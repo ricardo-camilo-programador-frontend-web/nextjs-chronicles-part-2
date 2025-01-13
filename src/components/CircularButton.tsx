dc3b4fcc4e1a79e9ecd95d66efcc2cf99fb5886b
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-13 16:01:23 -0300

    Refactor CircularButton component: Remove mouse movement effects and simplify structure. Update text content for improved clarity and user engagement. Enhance SVG animation for a more dynamic visual experience. This change streamlines the component's functionality and improves accessibility.

--- src/components/CircularButton.tsx
+++ src/components/CircularButton.tsx
@@ -12,41 +12,16 @@ interface CircularButtonProps {
 }
 
 export const CircularButton: FC<CircularButtonProps> = ({ id, color, colorClass }) => {
-  const buttonRef = useRef<HTMLDivElement>(null);
-  const textRef = useRef<HTMLDivElement>(null);
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (!buttonRef.current) return;
-
-    const rect = buttonRef.current.getBoundingClientRect();
-    const x = e.clientX - rect.left - rect.width / 2;
-    const y = e.clientY - rect.top - rect.height / 2;
-
-    buttonRef.current.style.transform = `translate(${x * 0.2}px, ${y * 0.2}px)`;
-    if (textRef.current) {
-      textRef.current.style.transform = `rotate(0deg)`;
-    }
-  };
-
-  const handleMouseLeave = () => {
-    if (!buttonRef.current) return;
-    buttonRef.current.style.transform = 'translate(0, 0)';
-    if (textRef.current) {
-      textRef.current.style.transform = `rotate(360deg)`;
-    }
-  };
-
   return (
     <div
       id={id}
       className="relative w-[100px] h-[100px] group"
     >
       <div
-        ref={textRef}
-        className="absolute inset-0 w-full h-full transition-transform duration-700 ease-in-out"
+        className="absolute inset-0 w-full h-full transition-all duration-700 ease-in-out"
         style={{ transformOrigin: 'center center' }}
       >
-        <svg viewBox="0 0 100 100" className="w-full h-full rotate-[-90deg] hidden group-hover:block">
+        <svg viewBox="0 0 100 100" className="w-full h-full transform-gpu rotate-[-90deg] hidden group-hover:block rotate(360deg) group-hover:text-white animate-spin duration-1000 ease-in-out">
           <path
             id="textCircle"
             d="M 50,50 m -37,0 a 37,37 0 1,1 74,0 a 37,37 0 1,1 -74,0"
@@ -55,16 +30,13 @@ export const CircularButton: FC<CircularButtonProps> = ({ id, color, colorClass 
           />
           <text className="text-[13px] uppercase">
             <textPath href="#textCircle" startOffset="0%">
-              Click to visit • Click to visit •
+              See More • Click to visit the project •
             </textPath>
           </text>
         </svg>
       </div>
 
       <div
-        ref={buttonRef}
-        onMouseMove={handleMouseMove}
-        onMouseLeave={handleMouseLeave}
         className={twMerge(
           'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
           'w-[70px] h-[70px] rounded-full flex items-center justify-center border-2',
