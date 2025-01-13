8194661c74579aa81b5897421589f0fbc2357ffa
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-13 10:27:20 -0300

    Implement localization for Testimonials section: Integrate translations for the title in both English and Brazilian Portuguese, enhancing internationalization support in the application.

--- src/components/sections/testimonials.tsx
+++ src/components/sections/testimonials.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { useTranslations } from 'next-intl';
 
 import { TESTIMONIALS } from '@/lib/data';
 import Client from '@/components/general/client';
 import Typography from '@/components/general/typography';
 
 const Testimonials = () => {
+  const t = useTranslations('testimonials');
+
   return (
     <section
       className="w-full flex justify-center items-center flex-col py-24 px-4 gap-24 bg-zinc-50 max-md:py-16"
       id="testimonials">
       <Typography variant="h2" className="uppercase w-full flex justify-center">
-        Experiências Memoráveis
+        {t('title')}
       </Typography>
       <div className="flex gap-16 max-lg:flex-col">
         {TESTIMONIALS.map((testimonial) => (
