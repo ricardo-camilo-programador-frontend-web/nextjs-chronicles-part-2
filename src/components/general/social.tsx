b54e1174858b1493a64716ca2ce84301abf9bee4
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-05 19:35:42 -0300

    UI: Add EnterAnimation to Social component with staggered entrance

--- src/components/general/social.tsx
+++ src/components/general/social.tsx
@@ -4,23 +4,30 @@ import { Fragment } from 'react';
 import { twMerge } from 'tailwind-merge';
 import Link from '@/components/general/link';
 import { SOCIAL_LINKS } from '@/lib/data';
+import EnterAnimation from '@/components/animation/EnterAnimation';
 
 const Social = ({ variant = 'dark' }: { variant?: 'dark' | 'light' }) => {
+  const randomFromZeroToNinetnine = Math.floor(Math.random() * 10)
   return (
     <div
       className={twMerge(
         'flex gap-5 text-md',
         variant === 'dark' ? 'text-zinc-200' : 'text-zinc-900'
       )}>
-      {SOCIAL_LINKS.map((link) => (
+      {SOCIAL_LINKS.map((link, index) => (
         <Fragment key={link.href}>
-          <Link
-            className="transform transition-all duration-300 ease-in-out hover:rotate-12 cursor-pointer hover:text-primary hover:scale-150"
-            variant={variant}
-            href={link.href}
-            externalLink>
-            <link.icon />
-          </Link>
+          <EnterAnimation
+            duration={0.4 + index * 0.1}
+            visualDuration={0.4 + index * 0.2}
+          >
+            <Link
+              className="transform transition-all duration-300 ease-in-out hover:rotate-12 cursor-pointer hover:text-primary hover:scale-150"
+              variant={variant}
+              href={link.href}
+              externalLink>
+              <link.icon />
+            </Link>
+          </EnterAnimation>
         </Fragment>
       ))}
     </div>
