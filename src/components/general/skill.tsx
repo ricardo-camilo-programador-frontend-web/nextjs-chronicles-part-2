9d5c1060496f3ee1148dac2d2ebd4199d4937654
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-05 17:46:32 -0300

    UI: Integrate RippleEffect into Skill component

--- src/components/general/skill.tsx
+++ src/components/general/skill.tsx
@@ -1,22 +1,32 @@
-import Image from 'next/image';
 import type { StaticImageData } from 'next/image';
-
+import Image from 'next/image';
 import Typography from '@/components/general/typography';
+import RippleEffect from '@/components/RippleEffect';
 
 interface SkillProps {
   label: string;
   icon: StaticImageData | string;
+  id: string;
   variant?: 'sm' | 'lg';
 }
 
-const Skill = ({ variant = 'sm', label, icon }: SkillProps) => {
+const Skill = ({ variant = 'sm', label, icon, id }: SkillProps) => {
+  const rippleId = `ripple-${id}`
+
   return (
-    <div className={'px-2 flex items-center gap-2 border border-zinc-200 rounded py-1 hover:bg-zinc-100 active:bg-zinc-200'}>
-      <Image src={icon} alt={label} className="w-6 h-6" />
-      <Typography variant={variant === 'sm' ? 'body2' : 'body1'}>
-        {label}
-      </Typography>
-    </div>
+    <>
+      <RippleEffect id={rippleId} />
+
+      <div
+        id={rippleId}
+        className={'relative px-2 flex items-center gap-2 border border-zinc-200 rounded py-1 hover:bg-zinc-100 active:bg-zinc-200 overflow-hidden'}
+      >
+        <Image src={icon} alt={label} className="w-6 h-6" />
+        <Typography variant={variant === 'sm' ? 'body2' : 'body1'}>
+          {label}
+        </Typography>
+      </div>
+    </>
   );
 };
 
