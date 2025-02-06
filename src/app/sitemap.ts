8af43e66c3e55e06a411aff2f45547183d6e2872
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-06 17:50:05 -0300

    SEO: Add dynamic sitemap generation for multilingual routes

---
+++ src/app/sitemap.ts
@@ -0,0 +1,36 @@
+import type { MetadataRoute } from 'next'
+import { locales, defaultLocale } from "@/config/i18n-config";
+
+const baseUrl = process.env.NEXT_PUBLIC_URL
+
+const routes = [
+  '',
+  'beyond-bio',
+]
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const sitemap: MetadataRoute.Sitemap = []
+
+  routes.forEach((route) => {
+    const urlPath = route ? `/${route}` : ''
+    const languageAlternates: { [key: string]: string } = {}
+
+    locales.forEach((lang) => {
+      const langPrefix = lang === defaultLocale ? '' : `/${lang}`
+
+      languageAlternates[lang] = `${baseUrl}${langPrefix}${urlPath}`
+    })
+
+    sitemap.push({
+      url: `${baseUrl}${urlPath}`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: route === '' ? 1 : 0.8,
+      alternates: {
+        languages: languageAlternates
+      }
+    })
+  })
+
+  return sitemap
+} 
